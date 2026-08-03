5b62d4834029f7738ca2da5475b1204ff9422bdf
anik78735-svg <anik78735@gmail.com>
2026-08-03 08:48:27 +0530

    Delete lib directory

--- lib/services/gallery_service_stub.dart
+++
@@ -1,6 +0,0 @@
-/// Stub used on mobile/desktop builds where dart:html is unavailable.
-/// This function is never actually called on those platforms because
-/// GalleryService checks kIsWeb first.
-void triggerBrowserDownload(String url, String fileName) {
-  throw UnsupportedError("Browser download is only available on web.");
-}
